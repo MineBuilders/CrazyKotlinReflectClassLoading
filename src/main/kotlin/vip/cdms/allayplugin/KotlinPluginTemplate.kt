bfe3cc079eed9392989af1a2a2e35397a31940e3
Cdm2883 <1759370644@qq.com>
2025-01-04 00:53:53 +0800

    Update KotlinPluginTemplate.kt

--- src/main/kotlin/vip/cdms/allayplugin/KotlinPluginTemplate.kt
+++ src/main/kotlin/vip/cdms/allayplugin/KotlinPluginTemplate.kt
@@ -1,10 +1,13 @@
 package vip.cdms.allayplugin
 
 import org.allaymc.api.plugin.Plugin
+import kotlin.reflect.full.findAnnotations
 
 @Suppress("unused")
 class KotlinPluginTemplate : Plugin() {
-    override fun onLoad() = pluginLogger.info("KotlinPluginTemplate loaded!")
+    override fun onLoad() {
+        pluginLogger.info("KotlinPluginTemplate loaded!" + KotlinPluginTemplate::class.findAnnotations<Suppress>())
+    }
     override fun onEnable() = pluginLogger.info("KotlinPluginTemplate enabled!")
     override fun onDisable() = pluginLogger.info("KotlinPluginTemplate disabled!")
 }
